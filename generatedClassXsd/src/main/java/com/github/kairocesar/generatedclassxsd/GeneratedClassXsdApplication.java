f7d482c500251ec8e967850d5870fd4c7195d862
Henrique Francelino de Souza <henrique0francelino0@gmail.com>
2024-04-23 16:28:47 -0300

    web service soap with spring boot

---
+++ generatedClassXsd/src/main/java/com/github/kairocesar/generatedclassxsd/GeneratedClassXsdApplication.java
@@ -0,0 +1,15 @@
+package com.github.kairocesar.generatedclassxsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratedClassXsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratedClassXsdApplication.class, args);
+
+
+	}
+
+}
---
+++ generatedClassXsd/src/test/java/com/github/kairocesar/generatedclassxsd/GeneratedClassXsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kairocesar.generatedclassxsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratedClassXsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
